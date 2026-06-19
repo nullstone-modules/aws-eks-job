a4d4490b7ca9a3262d02a77b75bb71c07f5e700e
Brad Sickles <brad.sickles@gmail.com>
2026-06-19 10:57:07 -0400

    use ns_workspace.k8s_labels

--- nullstone.tf
+++ nullstone.tf
@@ -23,38 +23,18 @@ locals {
   env_name      = data.ns_workspace.this.env_name
   resource_name = "${data.ns_workspace.this.block_ref}-${random_string.resource_suffix.result}"
 
+  app_labels = merge(data.ns_workspace.this.k8s_labels, {
+    "nullstone.io/app"          = local.app_name
+    "app.kubernetes.io/version" = local.app_version
+  })
+
+  component_labels = merge(data.ns_workspace.this.k8s_labels, {
+    "nullstone.io/app" = local.app_name
+  })
+
   match_labels = {
     "nullstone.io/stack" = local.stack_name
     "nullstone.io/app"   = local.app_name
     "nullstone.io/env"   = local.env_name
-  }
-
-  app_labels = {
-    // k8s-recommended labels
-    "app.kubernetes.io/name"       = local.block_name
-    "app.kubernetes.io/version"    = local.app_version
-    "app.kubernetes.io/component"  = ""
-    "app.kubernetes.io/part-of"    = local.stack_name
-    "app.kubernetes.io/managed-by" = "nullstone"
-    // nullstone labels
-    "nullstone.io/stack"     = local.stack_name
-    "nullstone.io/app"       = local.block_name
-    "nullstone.io/env"       = local.env_name
-    "nullstone.io/block-ref" = local.block_ref
-  }
-
-  component_labels = {
-    // k8s-recommended labels
-    "app.kubernetes.io/name"       = local.block_name
-    "app.kubernetes.io/version"    = ""
-    "app.kubernetes.io/component"  = ""
-    "app.kubernetes.io/part-of"    = data.ns_workspace.this.stack_name
-    "app.kubernetes.io/managed-by" = "nullstone"
-    // nullstone labels
-    "nullstone.io/stack"     = local.stack_name
-    "nullstone.io/block-ref" = local.block_ref
-    "nullstone.io/app"       = data.ns_workspace.this.block_name
-    "nullstone.io/block"     = local.block_name
-    "nullstone.io/env"       = local.env_name
   }
 }
